b676dfc9d4a39ead7eae22fcebc14737b6e5321b
Brian Lusina <lusinabrian@gmail.com>
2017-06-17 18:54:22 +0300

    add slack post url

--- app/mod_bot/views.py
+++ app/mod_bot/views.py
@@ -13,6 +13,7 @@ twilio_client = Client()
 def twilio_post():
     """
     Handles Twilio post requests. This post request will come from Twilio
+    :return Response from Twilio
     """
     response = twiml.TwiML()
     if request.form['From'] == current_app.config.get("USER_NUMBER"):
@@ -20,3 +21,23 @@ def twilio_post():
         slack_client.api_call("chat.postMessage", channel="#general",
                               text=message, username='butlerbot', icon_emoji=':robot_face:')
     return Response(response.to_xml(), mimetype="text/xml"), 200
+
+
+@bot.route("/slack", methods=["POST"])
+def slack_post():
+    """
+    This will handle post requests from Slack. This post request will come from Slack
+    When a message is sent to @butlerbot in a channel a POST request is sent to this app
+    and a message is then sent from Twilio to this application
+    :return:
+    """
+    if request.form["token"] == current_app.config.get("SLACK_WEBHOOK_SECRET"):
+        channel = request.form["channel_name"]
+        username = request.form["user_name"]
+        text = request.form["text"]
+        response_message = "{} in {} says {}".format(username, channel, text)
+        twilio_client.messages.create(to=current_app.config.get("USER_NUMBER"),
+                                      from_=current_app.config.get("TWILIO_NUMBER"),
+                                      body=response_message)
+    return Response(), 200
+
